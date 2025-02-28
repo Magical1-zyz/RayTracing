59bbd5372dae7cada06bc3aaddc841f079b05050
Yizhe Zhang <magical1_zyz@163.com>
2025-02-28 11:33:02 +0800

    add final scene and finished the next week

--- src/main.cpp
+++ src/main.cpp
@@ -402,11 +402,108 @@ void cornell_smoke() {
   cam.render(world);
 }
 
+void final_scene(int image_width, int samples_per_pixel, int max_depth) {
+  // boxes1
+  hittable_list boxes1;
+  auto ground = make_shared<lambertian>(color(0.48, 0.83, 0.53));
+
+  int boxes_per_side = 20;
+  for (int i = 0; i < boxes_per_side; ++i) {
+    for (int j = 0; j < boxes_per_side; ++j) {
+      auto w  = 100.0;
+      auto x0 = -1000.0 + i*w;
+      auto z0 = -1000.0 + j*w;
+      auto y0 = 0.0;
+      auto x1 = x0 + w;
+      auto y1 = random_double(1, 101);
+      auto z1 = z0 + w;
+
+      boxes1.add(box(point3(x0, y0, z0), point3(x1, y1, z1), ground));
+    }
+  }
+
+  // World
+  hittable_list world;
+
+  // BVH acceleration for boxes1
+  world.add(make_shared<bvh_node>(boxes1));
+
+  // light
+  auto light = make_shared<diffuse_light>(color(7, 7, 7));
+  world.add(make_shared<quad>(point3(123, 554, 147), vec3(412, 0, 0),
+                              vec3(0, 0, 412), light));
+
+  // motion globe
+  auto center1 = point3(400, 400, 200);
+  auto center2 = center1 + vec3(30, 0, 0);
+  auto sphere_material = make_shared<lambertian>(color(0.7, 0.3, 0.1));
+  world.add(make_shared<sphere>(center1, center2, 50, sphere_material));
+
+  // glass
+  world.add(make_shared<sphere>(point3(260, 150, 45), 50,
+                                make_shared<dielectric>(1.5)));
+  // metal
+  world.add(make_shared<sphere>(point3(0, 150, 145), 50,
+                                make_shared<metal>(color(0.8, 0.8, 0.9), 1.0)));
+
+  // smoke volume
+  auto boundary = make_shared<sphere>(point3(360, 150, 145),
+                                      70,make_shared<dielectric>(1.5));
+  world.add(boundary);
+  world.add(make_shared<constant_medium>(boundary, 0.2, color(0.2, 0.4, 0.9)));
+  boundary = make_shared<sphere>(point3(0, 0, 0),
+                                 5000, make_shared<dielectric>(1.5));
+  world.add(make_shared<constant_medium>(boundary, .0001, color(1, 1, 1)));
+
+  // Earth
+  auto emat = make_shared<lambertian>(
+      make_shared<image_texture>("../assets/textures/earthmap.jpg"));
+  world.add(make_shared<sphere>(point3(400, 200, 400), 100, emat));
+
+  // Perlin noise
+  auto pertext = make_shared<noise_texture>(0.2);
+  world.add(make_shared<sphere>(point3(220, 280, 300), 80,
+                                make_shared<lambertian>(pertext)));
+
+  // boxes2
+  hittable_list boxes2;
+  auto white = make_shared<lambertian>(color(.73, .73, .73));
+  int ns = 1000;
+  for (int j = 0; j < ns; ++j) {
+    boxes2.add(make_shared<sphere>(point3::random(0, 165), 10, white));
+  }
+
+  // Translation, rotation, and BVH acceleration
+  world.add(make_shared<translate>(
+      make_shared<rotate_y>(
+          make_shared<bvh_node>(boxes2), 15),
+          vec3(-100, 270, 395)));
+
+  // Camera
+  camera cam;
+
+  cam.aspect_ratio      = 1.0;
+  cam.image_width       = image_width;
+  cam.samples_per_pixel = samples_per_pixel;
+  cam.max_depth         = max_depth;
+  cam.background_color  = color(0, 0, 0);
+
+  cam.vfov              = 40;
+  cam.lookfrom          = point3(478, 278, -600);
+  cam.lookat            = point3(278, 278, 0);
+  cam.vup               = vec3(0, 1, 0);
+
+  cam.defocus_angle     = 0;
+
+  // Render
+  cam.render(world);
+}
+
 int main() {
     // time
     auto start_time = std::chrono::high_resolution_clock::now();
 
-  switch (8) {
+  switch (9) {
     // Choose the scene to render.
     case 1: // Bouncing Spheres
       bouncing_spheres();
@@ -431,6 +528,12 @@ int main() {
         break;
     case 8: // Cornell Box with Smoke
         cornell_smoke();
+        break;
+    case 9: // Final Scene
+        final_scene(800, 10000, 40);
+        break;
+    default: // Final Scene
+        final_scene(400, 250, 4);
         break;
   }
 
